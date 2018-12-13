b6e0874d0d8f5e0484934eb53579abf1d794be3f
Che Vladimir <iamcomputers@mail.ru>
2018-12-13 16:10:56 +1100

    changed price.php

--- PHP/price.php
+++ PHP/price.php
@@ -18,7 +18,26 @@
                   $tempArray = $row;
                   array_push($resultArray, $tempArray);
                }
-               echo json_encode($resultArray, JSON_UNESCAPED_UNICODE);
+               if (mysqli_num_rows($result) != 0) {
+                  echo json_encode($resultArray, JSON_UNESCAPED_UNICODE);
+               }
+               if (mysqli_num_rows($result) == 0) {
+                  $sql = "SELECT номенклатура.Цена FROM номенклатура
+                   WHERE номенклатура.Наименование LIKE '$itemName' ";
+                  if ($result = mysqli_query($dbconnect, $sql)) {
+                    while($row = $result->fetch_object()) {
+                      $tempArray = $row;
+                      array_push($resultArray, $tempArray);
+                    }
+                    if (mysqli_num_rows($result) != 0) {
+                      echo json_encode($resultArray, JSON_UNESCAPED_UNICODE);
+                    }
+                    if (mysqli_num_rows($result) == 0) {
+                      $json['ошибка'] = 'Пустой результат';
+                      echo json_encode($json, JSON_UNESCAPED_UNICODE);
+                    }
+                  }
+               }
                mysqli_close($dbconnect);
             }else{
                $json['ошибка'] = 'Что-то пошло не так';
