dc4865428e30b0cdd1af0393cd03d0742dc88aaa
Che Vladimir <iamcomputers@mail.ru>
2019-01-15 17:51:07 +1100

    start working on local db. created syncDB.php

---
+++ PHP/syncDB.php
@@ -0,0 +1,23 @@
+<?php
+  include("dbconnect.php");
+
+  if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $sql = "SELECT ID, Наименование, Район, Учет, DayOfTheWeek, Автор FROM salespartners ";
+
+    if ($result = mysqli_query($dbconnect, $sql)){
+      $resultArray = array();
+      $tempArray = array();
+      while($row = $result->fetch_object()){
+        $tempArray = $row;
+        array_push($resultArray, $tempArray);
+      }
+      echo json_encode($resultArray, JSON_UNESCAPED_UNICODE);
+      mysqli_close($dbconnect);
+    } else {
+      $json["failed"] = 'Login failed. Invalid login
+      and/or password';
+      echo json_encode($json, JSON_UNESCAPED_UNICODE);
+      mysqli_close($dbconnect);
+    }
+  }
+?>
