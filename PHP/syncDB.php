531249226077af8d982ce51d77dc767bd0bc473b
Che Vladimir <iamcomputers@mail.ru>
2019-04-02 20:03:12 +1100

    something

--- PHP/syncDB.php
+++ PHP/syncDB.php
@@ -25,7 +25,7 @@
       }
     }
     if($tableName == "items"){
-      $sql = "SELECT Артикул, Наименование, Цена, Описание FROM номенклатура ";
+      $sql = "SELECT Артикул, Наименование, Цена, Описание, Артикул_1С FROM номенклатура ";
 
       if ($result = mysqli_query($dbconnect, $sql)){
         $resultArray = array();
