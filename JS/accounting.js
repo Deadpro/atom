d9c173058393ea1a9d6f27473cbce11e5b835962
Che Vladimir <iamcomputers@mail.ru>
2019-06-25 16:06:45 +1100

    continue working on accountant system.

--- JS/accounting.js
+++ JS/accounting.js
@@ -12,15 +12,51 @@ var accountingLocalVars = {
   "taxPayerID" : "ИНН",
   "date" : "Дата",
   "salesPartnerOfficialName" : "Организация",
-  "dateStart" : "Начало (год-месяц-день часы:минуты):",
-  "dateEnd" : "Конец (год-месяц-день часы:минуты):",
+  "dateStartLabel" : "Начало (год-месяц-день часы:минуты):",
+  "dateEndLabel" : "Конец (год-месяц-день часы:минуты):",
+  "dateStart" : "",
+  "dateEnd" : "",
   "dash" : "---",
   "choosePeriod" : "Выберите период",
-  "chooseArea" : "Выберите район"
+  "chooseArea" : "Выберите район",
+  "checkRadio" : ["checkOne", "checkTwo", "checkThree", "checkFour", "checkFive"],
+  "checkedValue" : "",
+  "tmp" : new Object(),
+  "accounting" : "1"
+
 };
 
 $('#accounting').on('click', function() {
   renderAccountingOptions();
+});
+
+$('#executeChoice').on('click', function() {
+  // alert(document.getElementById(accountingLocalVars.checkRadio[0]).value);
+  for (var i = 0; i < 5; i++) {
+    if (document.getElementById(accountingLocalVars.checkRadio[i]).checked == true) {
+      checkedValue = document.getElementById(accountingLocalVars.checkRadio[i]).value;
+    }
+  }
+  accountingLocalVars.dateStart = $('input#dateStart').val();
+  accountingLocalVars.dateEnd = $('input#dateEnd').val();
+  $.post('../php/receiveReportData.php', {dbName: localStorage.getItem('dbName'), dbUser: localStorage.getItem('dbUser'),
+                                          dbPassword: localStorage.getItem('dbPassword'), dateStart: accountingLocalVars.dateStart,
+                                          dateEnd: accountingLocalVars.dateEnd, area: accountingLocalVars.checkedValue, accounting: accountingLocalVars.accounting}, function(data) {
+    accountingLocalVars.tmp = JSON.parse(data);
+    for (var i = 0; i < Object.keys(accountingLocalVars.tmp).length; i++) {
+      alert(Object.keys(accountingLocalVars.tmp).length);
+      alert(accountingLocalVars.tmp[i].Наименование);
+   //    trigger = false;
+   //    if (Object.keys(salesQuantity).length > 0) {
+   //      for (var key in salesQuantity) {
+   //      }
+   //      if (trigger == false) {
+   //      }
+   //    } else {
+   //    }
+   }
+   //  renderReportTable(0);
+  });
 });
 
 this.renderAccountingOptions = function() {
@@ -30,18 +66,18 @@ this.renderAccountingOptions = function() {
       <div class='panel panel-custom border'> \
         <div class='panel-heading col-100'><span>" + accountingLocalVars.choosePeriod + "</span></div> \
         <div class='panel-body'> \
-          <div class='col-60'>" + accountingLocalVars.dateStart + "</div><div class='col-40'><input type='text' id='dateStart'></div> \
-          <div class='col-60'>" + accountingLocalVars.dateEnd + "</div><div class='col-40'><input type='text' id='dateEnd'></div> \
+          <div class='col-60'>" + accountingLocalVars.dateStartLabel + "</div><div class='col-40'><input type='text' id='dateStart'></div> \
+          <div class='col-60'>" + accountingLocalVars.dateEndLabel + "</div><div class='col-40'><input type='text' id='dateEnd'></div> \
         </div> \
       </div> \
       <div class='panel panel-custom border'> \
         <div class='panel-heading col-100'><span>" + accountingLocalVars.chooseArea + "</span></div> \
         <div class='panel-body'> \
-           <div class='radioContainer'><input type='radio' id='checkOne' name='chooseone' value='Район 1'><label for='Район 1' id='radioLabel'>Район 1</label></div> \
-           <div class='radioContainer'><input type='radio' id='checkTwo' name='chooseone' value='Район 2'><label for='Район 2' id='radioLabel'>Район 2</label></div> \
-           <div class='radioContainer'><input type='radio' id='checkThree' name='chooseone' value='Район 3'><label for='Район 3' id='radioLabel'>Район 3</label></div> \
-           <div class='radioContainer'><input type='radio' id='checkFour' name='chooseone' value='Район 4'><label for='Район 4' id='radioLabel'>Район 4</label></div> \
-           <div class='radioContainer'><input type='radio' id='checkFive' name='chooseone' value='Район 5'><label for='Район 5' id='radioLabel'>Район 5</label></div> \
+           <div class='radioContainer'><input type='radio' id='checkOne' name='chooseone' value='1'><label for='Район 1' id='radioLabel'>Район 1</label></div> \
+           <div class='radioContainer'><input type='radio' id='checkTwo' name='chooseone' value='2'><label for='Район 2' id='radioLabel'>Район 2</label></div> \
+           <div class='radioContainer'><input type='radio' id='checkThree' name='chooseone' value='3'><label for='Район 3' id='radioLabel'>Район 3</label></div> \
+           <div class='radioContainer'><input type='radio' id='checkFour' name='chooseone' value='4'><label for='Район 4' id='radioLabel'>Район 4</label></div> \
+           <div class='radioContainer'><input type='radio' id='checkFive' name='chooseone' value='5'><label for='Район 5' id='radioLabel'>Район 5</label></div> \
         </div> \
       </div> \
       <div class='panel panel-custom border'> \
@@ -50,9 +86,9 @@ this.renderAccountingOptions = function() {
     </div> \
     <script src='../js/accounting.js' type='text/javascript' ></script> \
   ");
-  if (dateStart != "" && dateEnd != "") {
-     $('input#dateStart').val(dateStart);
-     $('input#dateEnd').val(dateEnd);
+  if (accountingLocalVars.dateStart != "" && accountingLocalVars.dateEnd != "") {
+     $('input#dateStart').val(accountingLocalVars.dateStart);
+     $('input#dateEnd').val(accountingLocalVars.dateEnd);
   }
   $(".loginContainer").html("");
   $(".loginContainer").hide();
--- PHP/receiveReportData.php
+++ PHP/receiveReportData.php
@@ -6,7 +6,9 @@
     $login = (trim($_POST['login']));
     $password = (trim($_POST['password']));
     // mysql_real_escape_string
-    // echo($login);
+    $accounting = trim($_POST['accounting']);
+    $area = trim($_POST['area']);
+    $index = (int)$area - 1;
     date_default_timezone_set("UTC"); // Устанавливаем часовой пояс по Гринвичу
     $time = time(); // Вот это значение отправляем в базу
     $time += 11 * 3600; // Добавляем 11 часов к времени по Гринвичу
@@ -16,7 +18,11 @@
     $currDate = date("Y-m-d H:i:s");
     $currDate = strtotime($dateTimeDoc);
     $date = strtotime($dateTimeDoc);
-    $date = strtotime("-4 day", $date);
+    if ($accounting == "1") {
+      $date = date('Y-m-d', $date);
+    } else {
+      $date = strtotime("-4 day", $date);
+    }
     // $dateTime = date('Y-m-d H:i:s', $date);
     // $dateTime = "2019-06-10 00:00:00";
     if (empty($_POST['dateStart']) === false && empty($_POST['dateEnd']) === false) {
@@ -31,13 +37,36 @@
     $areaArray[2] = 'invoice_three';
     $areaArray[3] = 'invoice_four';
     $areaArray[4] = 'invoice_five';
+
     $resultArray = array();
     $tempArray = array();
-    for ($i = 0; $i < 5; $i++) {
-      $areaArrayTmp = $areaArray[$i];
-      $sql = "SELECT ID, InvoiceNumber, AgentID, SalesPartnerID, AccountingType,
-      ItemID, Quantity, Price, Total, ExchangeQuantity, ReturnQuantity, DateTimeDocLocal,
-      InvoiceSum, номенклатура.Наименование FROM $areaArrayTmp INNER JOIN номенклатура ON $areaArrayTmp.ItemID = номенклатура.Артикул WHERE DateTimeDocLocal BETWEEN '$dateStart' AND '$dateEnd'  ORDER BY ItemID";
+    if ($accounting != '1') {
+      for ($i = 0; $i < 5; $i++) {
+        $areaArrayTmp = $areaArray[$i];
+        $sql = "SELECT ID, InvoiceNumber, AgentID, SalesPartnerID, AccountingType,
+        ItemID, Quantity, Price, Total, ExchangeQuantity, ReturnQuantity, DateTimeDocLocal,
+        InvoiceSum, номенклатура.Наименование FROM $areaArrayTmp INNER JOIN номенклатура
+        ON $areaArrayTmp.ItemID = номенклатура.Артикул
+        WHERE DateTimeDocLocal BETWEEN '$dateStart' AND '$dateEnd'  ORDER BY ItemID";
+        if ($result = mysqli_query($dbconnect, $sql)){
+          while($row = $result->fetch_object()){
+            $tempArray = $row;
+            array_push($resultArray, $tempArray);
+          }
+        } else {
+          $json["failed"] = 'Login failed. Invalid login
+          and/or password';
+          echo json_encode($json, JSON_UNESCAPED_UNICODE);
+          mysqli_close($dbconnect);
+        }
+      }
+    } else {
+      $areaArrayTmp = $areaArray[(int)$index];
+      $sql = "SELECT InvoiceNumber, AgentID, SalesPartnerID, AccountingType,
+      ItemID, Quantity, Price, Total, DateTimeDocLocal, InvoiceSum,
+      salespartners.Наименование, salespartners.ИНН, salespartners.ID FROM $areaArrayTmp
+      INNER JOIN salespartners ON $areaArrayTmp.SalesPartnerID = salespartners.ID
+      WHERE (DateTimeDocLocal BETWEEN '$dateStart' AND '$dateEnd')  AND AccountingType LIKE 'провод' ";
       if ($result = mysqli_query($dbconnect, $sql)){
         while($row = $result->fetch_object()){
           $tempArray = $row;
