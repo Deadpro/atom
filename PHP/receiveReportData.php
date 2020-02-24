66f90c9cbd615cb37ad1098c8c072172e5b29ea7
Che Vladimir <iamcomputers@mail.ru>
2020-02-24 13:35:48 +1100

    finished report by sp.

--- PHP/receiveReportData.php
+++ PHP/receiveReportData.php
@@ -131,26 +131,26 @@
       }
 
       if ($salesPartnerTrigger === true && $areaTrigger === false) {
-        $areaArrayTmp = $areaArray[4];
-        $sql = "SELECT ID, InvoiceNumber, AgentID, SalesPartnerID, AccountingType,
-        ItemID, Quantity, Price, Total, ExchangeQuantity, ReturnQuantity, DateTimeDocLocal,
-        InvoiceSum, номенклатура.Наименование FROM
-        invoice_one INNER JOIN invoice_two
-        INNER JOIN invoice_three INNER JOIN invoice_four INNER JOIN
-        $areaArrayTmp INNER JOIN номенклатура
-        ON invoice_five.ItemID = номенклатура.Артикул
-        WHERE (DateTimeDocLocal BETWEEN '$dateStart' AND '$dateEnd')  AND SalesPartnerID LIKE '$salesPartnerID'
-        ORDER BY ItemID";
-        if ($result = mysqli_query($dbconnect, $sql)){
-          while($row = $result->fetch_object()){
-            $tempArray = $row;
-            array_push($resultArray, $tempArray);
+        for ($i = 1; $i < 6; $i++) {
+          $areaArrayTmp = $areaArray[$i];
+          $sql = "SELECT ID, InvoiceNumber, AgentID, SalesPartnerID, AccountingType,
+          ItemID, Quantity, Price, Total, ExchangeQuantity, ReturnQuantity, DateTimeDocLocal,
+          InvoiceSum, номенклатура.Наименование FROM
+          $areaArrayTmp INNER JOIN номенклатура
+          ON $areaArrayTmp.ItemID = номенклатура.Артикул
+          WHERE (DateTimeDocLocal BETWEEN '$dateStart' AND '$dateEnd')  AND SalesPartnerID LIKE '$salesPartnerID'
+          ORDER BY ItemID";
+          if ($result = mysqli_query($dbconnect, $sql)){
+            while($row = $result->fetch_object()){
+              $tempArray = $row;
+              array_push($resultArray, $tempArray);
+            }
+          } else {
+            $json["failed"] = 'Login failed. Invalid login
+            and/or password';
+            echo json_encode($json, JSON_UNESCAPED_UNICODE);
+            mysqli_close($dbconnect);
           }
-        } else {
-          $json["failed"] = 'Login failed. Invalid login
-          and/or password';
-          echo json_encode($json, JSON_UNESCAPED_UNICODE);
-          mysqli_close($dbconnect);
         }
       }
     }
