7db0f125e2d1e15bb5395b149cab837253b85f7a
Che Vladimir <iamcomputers@mail.ru>
2020-03-05 14:04:15 +1100

    continue working on map. figuring out data json.

--- JS/request_map.js
+++ JS/request_map.js
@@ -28,6 +28,20 @@ this.renderMap = function() {
 // Инициализация и уничтожение карты при нажатии на кнопку.
 function init () {
     var myMap;
+    objectManager = new ymaps.ObjectManager({
+        // Чтобы метки начали кластеризоваться, выставляем опцию.
+        clusterize: true,
+        geoObjectOpenBalloonOnClick: false,
+        clusterOpenBalloonOnClick: false
+    });
+
+    myMap.geoObjects.add(objectManager);
+
+    $.ajax({
+        url: "data.json"
+    }).done(function(data) {
+        objectManager.add(data);
+    });
 
     $('#toggle').bind({
         click: function () {
@@ -49,4 +63,71 @@ function init () {
             }
         }
     });
+}
+
+let student = {
+  name: 'John',
+  age: 30,
+  isAdmin: false,
+  courses: ['html', 'css', 'js'],
+  wife: null
+};
+
+let json = JSON.stringify(student);
+
+alert(typeof json); // мы получили строку!
+
+alert(json);
+
+function initTest () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.76, 37.64],
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        objectManager = new ymaps.ObjectManager({
+            // Чтобы метки начали кластеризоваться, выставляем опцию.
+            clusterize: true,
+            geoObjectOpenBalloonOnClick: false,
+            clusterOpenBalloonOnClick: false
+        });
+
+    myMap.geoObjects.add(objectManager);
+
+    $.ajax({
+        url: "data.json"
+    }).done(function(data) {
+        objectManager.add(data);
+    });
+
+    function onObjectEvent (e) {
+        var objectId = e.get('objectId');
+        if (e.get('type') == 'mouseenter') {
+            // Метод setObjectOptions позволяет задавать опции объекта "на лету".
+            objectManager.objects.setObjectOptions(objectId, {
+                preset: 'islands#yellowIcon'
+            });
+        } else {
+            objectManager.objects.setObjectOptions(objectId, {
+                preset: 'islands#blueIcon'
+            });
+        }
+    }
+
+    function onClusterEvent (e) {
+        var objectId = e.get('objectId');
+        if (e.get('type') == 'mouseenter') {
+            objectManager.clusters.setClusterOptions(objectId, {
+                preset: 'islands#yellowClusterIcons'
+            });
+        } else {
+            objectManager.clusters.setClusterOptions(objectId, {
+                preset: 'islands#blueClusterIcons'
+            });
+        }
+    }
+
+    objectManager.objects.events.add(['mouseenter', 'mouseleave'], onObjectEvent);
+    objectManager.clusters.events.add(['mouseenter', 'mouseleave'], onClusterEvent);
 }
