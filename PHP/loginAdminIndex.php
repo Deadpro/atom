fe622486e789ea80f0230f6999ccbc2c574a6467
Che Vladimir <iamcomputers@mail.ru>
2021-01-08 13:38:40 +1100

    minor changes to last endevor.

--- PHP/loginAdminIndex.php
+++ PHP/loginAdminIndex.php
@@ -37,7 +37,7 @@
         <li><input type="submit" id="analytics" value="АНАЛИТИКА"></li>
         <li><input type="submit" id="printReport" value="ПЕЧАТЬ"></li>
         <li><input type="submit" id="changeDataBaseTables" value="ИЗМЕНИТЬ"></li>
-        <li><input type="submit" id="changeDataBaseTables" value="СКЛАД"></li>
+        <li><input type="submit" id="warehouse" value="СКЛАД"></li>
         <li><input type="submit" id="mapCaiman" value="КАРТА"></li>
         <li><input type="submit" id="adminSettings" value="НАСТРОЙКИ"></li>
       </ul>
