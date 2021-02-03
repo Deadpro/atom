d98bfd2aea5529dd66e1817c1cca9ea70f049e96
Che Vladimir <iamcomputers@mail.ru>
2021-02-03 14:12:42 +1100

    added clone tables.

--- PHP/saveNewInvoice_new.php
+++ PHP/saveNewInvoice_new.php
@@ -12,11 +12,13 @@
       if (mysqli_num_rows($result) != 0) {
         $tableName = $row['tableName'];
         $postfix = $row['tableName'];
+        $clone = $row['tableName'];
       }
     }
   }
 
   $postfix.="_twin";
+  $clone.="_clone";
 
   date_default_timezone_set("UTC"); // Устанавливаем часовой пояс по Гринвичу
   $time = time(); // Вот это значение отправляем в базу
@@ -61,7 +63,7 @@
     //   $tempArray = array('invoiceNumber' => $invoiceNumber, 'dateTimeDoc' => $dateTimeDoc);
     //   array_push($resultArray, $tempArray);
     // }
-
+    //
     // $sql = "SELECT ID FROM salespartners WHERE salespartners.Наименование LIKE '$salesPartnerName'
     // AND salespartners.Район LIKE '$areaSP' AND salespartners.Учет LIKE '$accountingTypeSP' ";
     // if ($result = mysqli_query($dbconnect, $sql)) {
@@ -82,71 +84,79 @@
     }
 
     $sql = "INSERT INTO $postfix (InvoiceNumber, AgentID, SalesPartnerID,
-     AccountingType, ItemID, Quantity, Price, Total, ExchangeQuantity,
-     ReturnQuantity, DateTimeDoc, InvoiceSum, Comment, InvoiceNumberLocal, DateTimeDocLocal)
-     VALUES ($invoiceNumber, $agentID, $salesPartnerID,
-     '$accountingTypeDoc', $itemID, $quantity, $price, $totalCost, $exchange,
-     $returns, '$dateTimeDoc', $invoiceSum, '$comment', $invoiceNumberLocal, '$dateTimeDocLocal') ";
+      AccountingType, ItemID, Quantity, Price, Total, ExchangeQuantity,
+      ReturnQuantity, DateTimeDoc, InvoiceSum, Comment, InvoiceNumberLocal, DateTimeDocLocal)
+      VALUES ($invoiceNumber, $agentID, $salesPartnerID,
+      '$accountingTypeDoc', $itemID, $quantity, $price, $totalCost, $exchange,
+      $returns, '$dateTimeDoc', $invoiceSum, '$comment', $invoiceNumberLocal, '$dateTimeDocLocal') ";
     mysqli_query($dbconnect, $sql);
 
     $sql = "SELECT COUNT(*) FROM $tableName WHERE InvoiceNumber LIKE $invoiceNumber AND ItemID LIKE $itemID
-    ORDER BY ID LIMIT 1000";
+      ORDER BY ID LIMIT 1000";
     // AgentID LIKE $agentID AND SalesPartnerID LIKE $salesPartnerID AND AccountingType LIKE '$accountingTypeDoc' AND
     //  AND Quantity LIKE $quantity AND Price LIKE $price AND Total LIKE $totalCost AND
     // ExchangeQuantity LIKE $exchange AND ReturnQuantity LIKE $returns AND InvoiceSum LIKE $invoiceSum AND
     // Comment LIKE '$comment' AND DateTimeDocLocal LIKE '$dateTimeDocLocal' LIMIT 1";
     if ($result = mysqli_query($dbconnect, $sql)) {
-        $row = mysqli_fetch_row($result);
-          $totalMatches = $row[0];
+      $row = mysqli_fetch_row($result);
+      $totalMatches = $row[0];
     }
     if ($totalMatches == 0) {
-      // $resultArray = array();
-      // $tempArray = array();
+    // $resultArray = array();
+    // $tempArray = array();
       $sql = "INSERT INTO $tableName (InvoiceNumber, AgentID, SalesPartnerID,
         AccountingType, ItemID, Quantity, Price, Total, ExchangeQuantity,
-       ReturnQuantity, DateTimeDoc, InvoiceSum, Comment, InvoiceNumberLocal, DateTimeDocLocal)
-       VALUES ($invoiceNumber, $agentID, $salesPartnerID,
-         '$accountingTypeDoc', $itemID, $quantity, $price, $totalCost, $exchange,
-         $returns, '$dateTimeDoc', $invoiceSum, '$comment', $invoiceNumberLocal, '$dateTimeDocLocal') ";
+        ReturnQuantity, DateTimeDoc, InvoiceSum, Comment, InvoiceNumberLocal, DateTimeDocLocal)
+        VALUES ($invoiceNumber, $agentID, $salesPartnerID,
+        '$accountingTypeDoc', $itemID, $quantity, $price, $totalCost, $exchange,
+        $returns, '$dateTimeDoc', $invoiceSum, '$comment', $invoiceNumberLocal, '$dateTimeDocLocal') ";
 
       if (mysqli_query($dbconnect, $sql)) {
          // $tmpInfo = "New record created successfully";
          // $tempArray = array('requestMessage' => $tmpInfo);
          // array_push($resultArray, $tempArray);
+         $sqlClone = "INSERT INTO $tableName (InvoiceNumber, AgentID, SalesPartnerID,
+           AccountingType, ItemID, Quantity, Price, Total, ExchangeQuantity,
+           ReturnQuantity, DateTimeDoc, InvoiceSum, Comment, InvoiceNumberLocal, DateTimeDocLocal)
+           VALUES ($invoiceNumber, $agentID, $salesPartnerID,
+           '$accountingTypeDoc', $itemID, $quantity, $price, $totalCost, $exchange,
+           $returns, '$dateTimeDoc', $invoiceSum, '$comment', $invoiceNumberLocal, '$dateTimeDocLocal') ";
+         if (mysqli_query($dbconnect, $sqlClone)) {
+         }
       } else {
          echo "Error: " . $sql . "<br>" . mysqli_error($dbconnect);
          $tmpInfo = "Error";
       }
-   }
+    }
   }
   // $itemsSoldCount = 0;
   // if ($tmpInfo == "New record created successfully") {
-    $sql = "SELECT DISTINCT InvoiceNumber FROM $tableName WHERE DateTimeDoc LIKE '$dateTimeDoc' ";
-    if ($result = mysqli_query($dbconnect, $sql)) {
-       $resultArray = array();
-       $tempArray = array();
-       while($row = $result->fetch_object()) {
-          $tempArray = $row;
-          array_push($resultArray, $tempArray);
-       }
-       echo json_encode($resultArray, JSON_UNESCAPED_UNICODE);
-       mysqli_close($dbconnect);
-    }
-    // $sql = "SELECT InvoiceNumber FROM $tableName WHERE DateTimeDoc LIKE '$dateTimeDoc' ";
-    // if ($result = mysqli_query($dbconnect, $sql)) {
-    //    // $resultArray = array();
-    //    $tempArray = array();
-    //    while($row = $result->fetch_object()) {
-    //       $itemsSoldCount += 1;
-    //    }
-    //    $tempArray = array('itemsSoldCount' => $itemsSoldCount);
-    //    array_push($resultArray, $tempArray);
-    //    echo json_encode($resultArray, JSON_UNESCAPED_UNICODE);
-    //    // mysqli_close($dbconnect);
-    // }
-    // $tempArray = array('requestMessage' => $tmpInfo);
-    // array_push($resultArray, $tempArray);
-    // echo json_encode($resultArray, JSON_UNESCAPED_UNICODE);
+  $sql = "SELECT DISTINCT InvoiceNumber FROM $tableName WHERE DateTimeDoc LIKE '$dateTimeDoc' ";
+  if ($result = mysqli_query($dbconnect, $sql)) {
+     $resultArray = array();
+     $tempArray = array();
+     while($row = $result->fetch_object()) {
+        $tempArray = $row;
+        array_push($resultArray, $tempArray);
+     }
+     echo json_encode($resultArray, JSON_UNESCAPED_UNICODE);
+     mysqli_close($dbconnect);
+  }
+  //   $sql = "SELECT InvoiceNumber FROM $tableName WHERE DateTimeDoc LIKE '$dateTimeDoc' ";
+  //   if ($result = mysqli_query($dbconnect, $sql)) {
+  //      // $resultArray = array();
+  //      $tempArray = array();
+  //      while($row = $result->fetch_object()) {
+  //         $itemsSoldCount += 1;
+  //      }
+  //      $tempArray = array('itemsSoldCount' => $itemsSoldCount);
+  //      array_push($resultArray, $tempArray);
+  //      echo json_encode($resultArray, JSON_UNESCAPED_UNICODE);
+  //      // mysqli_close($dbconnect);
+  //   }
+  //   $tempArray = array('requestMessage' => $tmpInfo);
+  //   array_push($resultArray, $tempArray);
+  //   echo json_encode($resultArray, JSON_UNESCAPED_UNICODE);
   // }
-   // echo json_encode($resultArray, JSON_UNESCAPED_UNICODE);
+  //  echo json_encode($resultArray, JSON_UNESCAPED_UNICODE);
 ?>
